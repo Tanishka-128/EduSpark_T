a8a718dbe16ca24bbb173f3714b2ece6f1e8a9e8
Tanishka <214400322+Tanishka-128@users.noreply.github.com>
2025-10-02 05:00:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5331507680-c4ca5",
+  "appId": "1:119576770123:web:4325dd5a7360fa04717fce",
+  "apiKey": "AIzaSyBDgRBiai3Hii4C2b5VY2oPLYtfVZBR_tw",
+  "authDomain": "studio-5331507680-c4ca5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "119576770123"
+};
